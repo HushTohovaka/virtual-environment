9c82636ede9e0775aedcf22c49542fa660a021a8
Hushdon Tohovaka [htt0040] <htt0040@arastudent.ac.nz>
2024-03-07 18:13:57 +1300

    Finished Example

---
+++ index.py
@@ -0,0 +1,12 @@
+# Create a function that says hello world.
+
+def hello_world():
+    print("Hello World!")   
+
+
+# Call the hello world function
+    hello_world()
+
+# create a while loop function that says hello world
+    while True:
+        print("Hello World!")
